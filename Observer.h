817126e1ffe7ee82fa96b0048ce5c39e2e383600
abir <39681215+abir006@users.noreply.github.com>
2020-01-15 14:59:22 +0200

    first files for project nothing is done.

---
+++ Observer.h
@@ -0,0 +1,12 @@
+//
+// Created by ארז on 15/01/2020.
+//
+
+#ifndef OOP_HW5_OBSERVER_H
+#define OOP_HW5_OBSERVER_H
+
+class Observer {
+
+};
+
+#endif //OOP_HW5_OBSERVER_H
---
+++ Subject.h
@@ -0,0 +1,12 @@
+//
+// Created by ארז on 15/01/2020.
+//
+
+#ifndef OOP_HW5_SUBJECT_H
+#define OOP_HW5_SUBJECT_H
+
+class Subject {
+
+};
+
+#endif //OOP_HW5_SUBJECT_H
