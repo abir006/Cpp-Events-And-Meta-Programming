3e38f615595a33d526c6c883c530ac3bd9c53ebf
Shahakbk <shahakbk@gmail.com>
2020-01-19 14:13:46 +0200

    added Direction & CellType enums

---
+++ part2/CellType.h
@@ -0,0 +1,14 @@
+//
+// Created by Shahak on 18/01/2020.
+//
+
+#ifndef OOP_HW5_CELLTYPE_H
+#define OOP_HW5_CELLTYPE_H
+
+enum CellType {
+    EMPTY,
+    X, A, B, C, D, E, F, G, H, I, J, K,
+    O, P, Q, R
+};
+
+#endif //OOP_HW5_CELLTYPE_H
---
+++ part2/Direction.h
@@ -0,0 +1,13 @@
+//
+// Created by Shahak on 18/01/2020.
+//
+
+#ifndef OOP_HW5_DIRECTION_H
+#define OOP_HW5_DIRECTION_H
+
+enum Direction {
+    UP = 0,
+    DOWN, LEFT, RIGHT
+};
+
+#endif //OOP_HW5_DIRECTION_H
